3339fa72b36e659e08f7567f4698f0a40b58e521
Yaligarchetan <122506313+Yaligarchetan@users.noreply.github.com>
2024-10-22 00:40:21 +0530

    removed KEY

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot= document.querySelector("#close-chatbot");
 
 
 //API setup
-const API_KEY="AIzaSyAQPiGLqKuK_LTqUGdGjZHyBus2DZ46JS8";
+const API_KEY="";
 const API_URL= `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 const userData={
 message:null,
